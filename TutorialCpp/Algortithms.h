b92ea8f1ececc1691be473fc2e1ebff827bc3d14
Бакаев Сергей <daspreichp88@gmail.com>
2021-02-04 09:47:35 +0200

    Add string algorithm

--- TutorialCpp/Algortithms.h
+++ TutorialCpp/Algortithms.h
@@ -39,5 +39,105 @@ struct Algorithms
 			ret.push_back(local);
 			return ret;
 		}
+
+		static std::vector<std::wstring> Split(std::wstring_view delim, const std::wstring& str)
+		{
+			std::vector<std::wstring> ret;
+			size_t pos = 0;
+			std::wstring local = str;
+			while ((pos = local.find(delim)) != sstring::npos)
+			{
+				std::wstring token = local.substr(0, pos);
+				ret.push_back(token);
+				local.erase(0, pos + delim.length());
+
+			}
+			ret.push_back(local);
+			return ret;
+		}
+
+		// trim from start (in place)
+		static inline void ltrim_local(std::string& s)
+		{
+			s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+				return !std::isspace(ch);
+				}));
+		}
+
+		// trim from start (in place)
+		static inline void ltrim_local(std::wstring& s)
+		{
+			s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+				return !std::isspace(ch);
+				}));
+		}
+
+		// trim from end (in place)
+		static inline void rtrim_local(std::string& s)
+		{
+			s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+				return !std::isspace(ch);
+				}).base(), s.end());
+		}
+
+		static inline void rtrim_local(std::wstring& s)
+		{
+			s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+				return !std::isspace(ch);
+				}).base(), s.end());
+		}
+
+		// trim from both ends (in place)
+		static inline void trim_local(std::string& s)
+		{
+			ltrim_local(s);
+			rtrim_local(s);
+		}
+
+		static inline void trim_local(std::wstring& s)
+		{
+			ltrim_local(s);
+			rtrim_local(s);
+		}
+
+		// trim from start (copying)
+		static inline std::string ltrim(std::string s) 
+		{
+			ltrim_local(s);
+			return s;
+		}
+
+		static inline std::wstring ltrim(std::wstring s)
+		{
+			ltrim_local(s);
+			return s;
+		}
+
+		// trim from end (copying)
+		static inline std::string rtrim(std::string s) 
+		{
+			rtrim_local(s);
+			return s;
+		}
+
+		// trim from end (copying)
+		static inline std::wstring rtrim(std::wstring s)
+		{
+			rtrim_local(s);
+			return s;
+		}
+
+		// trim from both ends (copying)
+		static inline std::string trim(std::string s)
+		{
+			trim_local(s);
+			return s;
+		}
+
+		static inline std::wstring trim(std::wstring s)
+		{
+			trim_local(s);
+			return s;
+		}
 	};
 };
--- TutorialCpp/TutorialCpp.cpp
+++ TutorialCpp/TutorialCpp.cpp
@@ -1,63 +1,209 @@
 #include <iostream>
 #include "kdtree.h"
 #include <vector>
+#include <thread>
+#include <chrono>
+#include <future>
+#include "Trigger.h"
 
-double sum()
+using namespace std::chrono_literals;
+
+#include "Algortithms.h"
+using DoubleVector = std::vector<double>;
+using DoubleVectors = std::vector<DoubleVector>;
+
+bool IsLessThanLimitVer(std::wstring curentVer, std::wstring limitVer)
 {
-    std::cout << "Sum\n";
-    return 0;
-}
+    Algorithms::String::trim_local(curentVer);
+    Algorithms::String::trim_local(limitVer);
 
-double fake_sum()
-{
-    std::cout << "Sum\n";
-    return 0;
-}
+    
+    std::vector<std::wstring> lexemCurentVer = Algorithms::String::Split(L".",curentVer);
+    if (lexemCurentVer.size() <= 2)
+        return false;
 
-//using FPtr = decltype(&Foo::sum);
+    if (lexemCurentVer.size() >= 3 && limitVer.empty())
+        return true;
 
-typedef double(*f)(void);
+    std::vector<std::wstring> lexemLimitVer = Algorithms::String::Split(L".", limitVer);
+    if (lexemLimitVer.size() <= 2)
+        return false;
 
-//struct Trigger
-//{
-//    FPtr f1_;
-//    FPtr f2_;
-//} trigger{ &Foo::sum,&Foo::fake_sum };
-
-struct Trigger2
-{
-    using Type1 = f;
-    using Type2 = f;
-    Type1 f1_;
-    Type2 f2_;
-};
-
-struct test
-{
-    __int64 t = 1;
-    __int64 t2 = 2;
-    __int64 t3 = 3;
-};
-template<typename T, typename R>
-void* void_cast(R(T::* f)())
-{
-    union
+    size_t size = lexemCurentVer.size();
+    bool rc = true;
+    for (size_t i = 0; i < size; i++)
     {
-        R(T::* pf)();
-        void* p;
-    };
-    pf = f;
-    return p;
+        long long cur = std::stoll(lexemCurentVer[i]);
+        long long lim = std::stoll(lexemLimitVer[i]);
+        if (cur > lim)
+        {
+            rc = false;
+            break;
+        }
+    }
+
+    return rc;
 }
-constexpr Trigger2 trigger2{ sum,fake_sum };
+
+
+
+struct Foo
+{
+    int a;
+    Foo() { a = 10; }
+    virtual ~Foo() = default;
+
+    void bar()
+    {
+        std::cout << "Foo::bar\n";
+    }
+
+    void fake_bar()
+    {
+        std::cout << "Foo::fake_bar\n";
+    }
+
+    double bar1()
+    {
+        std::cout << "Foo::bar1\n";
+        return 0.0;
+    }
+
+    double fake_bar1()
+    {
+        std::cout << "Foo::fake_bar1\n";
+        return 0.234340;
+    }
+
+
+    double bar2()
+    {
+        std::cout << "Foo::bar3\n";
+        return 0.0;
+    }
+
+    double fake_bar2()
+    {
+        std::cout << "Foo::fake_bar2\n";
+        return 0.0;
+    }
+
+    double bar3(double bar)
+    {
+        return bar * 2;
+    }
+
+    double fake_bar3(double bar)
+    {
+        return bar * 2;
+    }
+};
+
+
+//DECLARE_FUNC_TYPE(Foo::bar, TestBar);
+//DECLARE_FUNC_TYPE(Foo::bar, FakeBar);
+//
+//DECLARE_ROOT_TRIEGGER(Test, Foo::bar, Foo::fake_bar);
+//
+//
+//#define CALL_TRIGGER(NAME) __TrigerStruct__##NAME._Func1
+
+
+void watch_dog(bool stop)
+{
+    std::chrono::duration<double> delta = 3.5s;
+    while (!stop)
+    {
+        
+        auto start_sleep = std::chrono::system_clock::now();
+        std::this_thread::sleep_for(3s);
+        auto end_sleep = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end_sleep - start_sleep;
+        if (elapsed_seconds > delta)
+            std::cout << "Program in debug!!!\n";
+    }
+    
+
+}
+
+class WatchDog
+{
+public:
+
+    void Run()
+    {
+        _thread = std::thread(&WatchDog::Loop,this);
+    }
+
+    void Stop()
+    {
+        _stoped = true;
+    }
+
+private:
+    void Loop()
+    {
+        std::chrono::duration<double> delta = 3.5s;
+        while (!_stoped)
+        {
+
+            auto start_sleep = std::chrono::system_clock::now();
+            std::this_thread::sleep_for(3s);
+            auto end_sleep = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end_sleep - start_sleep;
+            if (elapsed_seconds > delta)
+                std::cout << "Program in debug!!!\n";
+        }
+    }
+
+private:
+    bool _stoped = false;
+    std::thread _thread;
+};
+
+void Test1()
+{
+    std::cout << "Test1\n";
+}
+
+void FakeTest1()
+{
+    std::cout << "Fake Test2\n";
+}
+
+struct Test
+{
+    
+};
+
+namespace A
+{
+    int test;
+}
+
+namespace B
+{
+    
+//    A::test = 3;
+    void ttt() { A::test = 3; };
+}
+
+extern "C" {
+    struct ghgh
+    {
+        int a;
+        int b;
+    }test;
+}
+
 int main()
 {
-    ((f)&trigger2)();
-    
-  
-
-
-
-    return 0;
+    int a[3];
+    std::cout << test.a;
+    std::cout << test.b;
+    A::test = 3;
+    //int z = 3;
+    //Test2 t;
+   // Visit(&__TrigerStruct__Test);
 }
 
